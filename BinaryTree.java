4d28076923aa1bd4b7f314e3000623b2897e888f
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2025-12-02 21:53:47 +0530

    check sub tree exist in main tree leet code 572

--- BinaryTree.java
+++ BinaryTree.java
@@ -78,6 +78,71 @@ public class BinaryTree {
         }
     }
 
+    public static int countofnodes(Node root){
+        if(root ==null){
+            return 0;
+        }
+        int leftNode = countofnodes(root.left);
+        int rightNode = countofnodes(root.right);
+        return leftNode + rightNode + 1;
+    }
+
+    public static int sumofnodes(Node root){
+        if(root ==null){
+            return 0;
+        }
+        int leftsum=sumofnodes(root.left);
+        int rightsum=sumofnodes(root.right);
+        return leftsum + rightsum + root.data;
+    }
+
+    public static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftheight=height(root.left);
+        int rightheight=height(root.right);
+        return Math.max(leftheight,rightheight)+1;
+    }
+    public static int diameter(Node root){
+        if(root==null){
+            return 0;
+        }
+        int diam1=diameter(root.left);
+        int diam2=diameter(root.right);
+        int diam3=height(root.left)+height(root.right)+1;
+        return Math.max(diam3,Math.max(diam1,diam2));
+    }
+    static class Info{
+        int diam;
+        int ht;
+
+        Info(int diam,int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+
+    public static Info diameter2(Node root){
+        if(root==null){
+            return new Info(0,0);
+        }
+        Info leftinfo=diameter2(root.left);
+        Info rightinfo=diameter2(root.right);
+
+        int diam1=leftinfo.diam;
+        int diam2=rightinfo.diam;
+        int diam3=leftinfo.ht + rightinfo.ht + 1;
+
+        int ht=Math.max(leftinfo.ht,rightinfo.ht)+1;
+
+        int finaldiam=Math.max(diam3,Math.max(diam1,diam2));
+        return new Info(finaldiam,ht);
+    }
+
+
+
+
 
 
     public static void main(String[] args){
@@ -91,5 +156,11 @@ public class BinaryTree {
         postorder(root);
         System.out.println();
         levelorder(root);
+        System.out.println("Count of nodes: "+countofnodes(root));
+        System.out.println("Sum of nodes: "+sumofnodes(root));
+        System.out.println("Height of tree: "+height(root));
+        System.out.println("Diameter of tree: "+diameter(root));
+        System.out.println("Diameter of tree (optimized): "+diameter2(root).diam);
+
     }
 }
