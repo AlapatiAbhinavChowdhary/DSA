f386aae2083196010539913d3126d0086051ade6
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2025-12-05 22:31:11 +0530

    BST

---
+++ BST.java
@@ -0,0 +1,44 @@
+public class BST {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    public static Node insert(Node root,int val){
+        if(root==null){
+            root = new Node(val);
+            return root;
+        }
+        if(val<root.data){
+            root.left=insert(root.left,val);
+        }
+        else{
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+
+    }
+  public static void main(String[] args){
+    int val[]={5,3,8,1,4,7,9};
+    Node root=null;
+    for(int i=0;i<val.length;i++){
+        root=insert(root,val[i]);
+    }
+    inorder(root);
+    System.out.println();
+  }  
+}
