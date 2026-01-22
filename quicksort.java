6a0cc23750ba76c3fdfb7c6be9dfa5c1fafc3cef
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-01-22 22:31:54 +0530

    quick sort

---
+++ quicksort.java
@@ -0,0 +1,13 @@
+public class quicksort {
+    public static void quick(int[] arr,int low,int high){
+        if(low<high){
+
+        }
+    }
+}
+public class main{
+    public static void main(String[] args){
+        int[] arr={10,7,8,9,1,5};
+        quicksort.quick(arr,0,arr.length-1);
+    }
+}
