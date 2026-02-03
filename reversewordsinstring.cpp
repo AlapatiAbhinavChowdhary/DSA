a90261cd5c161bde95c6d1de02d7c5df51171aa7
Alapati Abhinav Chowdhary <147315664+AlapatiAbhinavChowdhary@users.noreply.github.com>
2026-02-03 13:53:42 +0530

    Implement function to reverse words in a string

---
+++ reversewordsinstring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string>temp;
+        int n=s.length();
+        string word="";
+        for(int i=0;i<n;i++){
+            if(s[i]==' '){
+                if(word.length()>0){
+                    temp.push_back(word);
+                    word="";
+                }
+            }else{
+                word+=s[i];
+            }
+        }
+        if(word.length()>0){
+            temp.push_back(word);
+        }
+        string ans="";
+        for(int i=temp.size()-1;i>=0;i--){
+            if(i!=0){
+                ans+=temp[i];
+                ans+=" ";
+            }else{
+                ans+=temp[i];
+            }
+        }
+        return ans;
+    }
+};
