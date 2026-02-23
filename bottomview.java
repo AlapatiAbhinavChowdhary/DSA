ba4c2cc72b98ea186c885485122ee736b5678a5e
Alapati Abhinav Chowdhary <147315664+AlapatiAbhinavChowdhary@users.noreply.github.com>
2026-02-23 11:15:23 +0530

    Create bottomview.java

---
+++ bottomview.java
@@ -0,0 +1,53 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    static class pair{
+        Node node;
+        int level;
+        pair(Node node,int level){
+            this.node=node;
+            this.level=level;
+        }
+    }
+    public ArrayList<Integer> bottomView(Node root) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(root==null) return ans;
+        TreeMap<Integer,ArrayList<Integer>> map=new TreeMap<>();
+        Queue<pair> q=new LinkedList<>();
+        q.add(new pair(root,0));
+        while(!q.isEmpty()){
+            pair p=q.poll();
+            Node curr=p.node;
+            int lev=p.level;
+            if(!map.containsKey(lev)){
+                map.put(lev,new ArrayList<>());
+            }
+            map.get(lev).add(curr.data);
+            if(curr.left!=null){
+                q.add(new pair(curr.left,lev-1));
+            }
+            if(curr.right!=null){
+                q.add(new pair(curr.right,lev+1));
+            }
+            
+        }
+        for(int key:map.keySet()){
+            int size=map.get(key).size();
+            ans.add(map.get(key).get(size-1));
+        }
+        return ans;
+    }
+}
