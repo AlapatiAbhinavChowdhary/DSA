4fe7b9a18b0bcae8fc03d81b09cd0391788a3a66
Alapati Abhinav Chowdhary <147315664+AlapatiAbhinavChowdhary@users.noreply.github.com>
2026-02-23 10:55:37 +0530

    Create topView.java

---
+++ topView.java
@@ -0,0 +1,50 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+class Solution {
+    static class pair{
+        Node node;
+        int level;
+        pair(Node node,int level){
+            this.node=node;
+            this.level=level;
+        }
+    }
+    public ArrayList<Integer> topView(Node root) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(root==null) return ans;
+        TreeMap<Integer,ArrayList<Integer>> map=new TreeMap<>();
+        Queue<pair> q=new LinkedList<>();
+        q.add(new pair(root,0));
+        while(!q.isEmpty()){
+            pair p=q.poll();
+            Node curr=p.node;
+            int level=p.level;
+            if(!map.containsKey(level)){
+                map.put(level,new ArrayList<>());
+            }
+            map.get(level).add(curr.data);
+            if(curr.left!=null){
+                q.add(new pair(curr.left,level-1));
+            }
+            if(curr.right!=null){
+                q.add(new pair(curr.right,level+1));
+            }
+        }
+        for(int key:map.keySet()){
+            ans.add(map.get(key).get(0));
+        }
+        return ans;
+        
+    }
+}
