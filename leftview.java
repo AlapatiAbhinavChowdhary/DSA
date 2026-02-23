b2f54ceceaab1d74ebe3e295c45a77fd2db83b6e
Alapati Abhinav Chowdhary <147315664+AlapatiAbhinavChowdhary@users.noreply.github.com>
2026-02-23 09:52:05 +0530

    Implement left view of binary tree

---
+++ leftview.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ArrayList<Integer> leftView(Node root) {
+        // code here
+        ArrayList<Integer> arr=new ArrayList<>();
+        if(root==null) return arr;
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Node curr=q.poll();
+                if(i==0) arr.add(curr.data);
+                if(curr.left!=null)q.add(curr.left);
+                if(curr.right!=null)q.add(curr.right);
+            }
+        }
+        return arr;
+    }
+}
