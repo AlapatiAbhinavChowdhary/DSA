8052e28454442c2d3c6b36bd570ad77ddd6d4307
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-03-03 22:58:12 +0530

    Add SymmetricTree solution to Trees folder

---
+++ Graphs/FloodFill.java
@@ -0,0 +1,21 @@
+class Solution {
+    void dfs(int[][] image, int sr, int sc, int ne, int original) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length)
+            return;
+        if (image[sr][sc] != original)
+            return;
+        image[sr][sc] = ne;
+        dfs(image, sr + 1, sc, ne, original);
+        dfs(image, sr - 1, sc, ne, original);
+        dfs(image, sr, sc + 1, ne, original);
+        dfs(image, sr, sc - 1, ne, original);
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int original = image[sr][sc];
+        if (original == color)
+            return image;
+        dfs(image, sr, sc, color, original);
+        return image;
+    }
+}
