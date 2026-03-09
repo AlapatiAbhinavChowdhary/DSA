b90f63a72ad47235876ee536b652c5a4f4112fb5
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-03-09 22:53:54 +0530

    Add CheckIthBitOptimal - Bit Manipulation (optimal approach)

---
+++ BitManipulation/CheckIthBitOptimal.java
@@ -0,0 +1,15 @@
+class CheckIthBitOptimal {
+    public static boolean checkIthBit(int n, int i) {
+        return (n & (1 << i)) != 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int i = 2;
+        if (checkIthBit(n, i)) {
+            System.out.println("set");
+        } else {
+            System.out.println("not set");
+        }
+    }
+}
