6dfc765c725659176e361d358728384b2c82197f
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-03-09 22:51:15 +0530

    Add CheckIthBit - Bit Manipulation

---
+++ BitManipulation/CheckIthBit.java
@@ -0,0 +1,17 @@
+class CheckIthBit {
+    public static boolean checkIthBit(int n, int i) {
+        String binary = Integer.toBinaryString(n);
+        if (i > binary.length()) return false;
+        return binary.charAt(binary.length() - 1 - i) == '1';
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int i = 2;
+        if (checkIthBit(n, i)) {
+            System.out.println("set");
+        } else {
+            System.out.println("not set");
+        }
+    }
+}
