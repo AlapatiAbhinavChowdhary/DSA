39f287d809902b857de104b1444e774a4dfedc10
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-03-09 23:21:18 +0530

    Add RightmostUnsetToSet - Set rightmost unset bit using n|(n+1)

---
+++ BitManipulation/RightmostUnsetToSet.java
@@ -0,0 +1,11 @@
+class RightmostUnsetToSet {
+    public static int rightmosttoset(int n) {
+        return n | (n + 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(rightmosttoset(n));
+
+    }
+}
