c0ec3ab33da04d38682681023845d72cdf5a3831
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-03-09 23:17:30 +0530

    Add NoSetBits2 - Count set bits using right shift approach

---
+++ BitManipulation/NoSetBits2.java
@@ -0,0 +1,16 @@
+class NoSetBits2 {
+    public static int nosetbits(int n) {
+        int c = 0;
+        while (n > 0) {
+            c += (n & 1);
+            n >>= 1;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(nosetbits(n));
+
+    }
+}
