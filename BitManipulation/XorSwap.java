c6481de31322ecad420495bae1abb627314e1c7f
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-03-09 23:34:53 +0530

    Add XorSwap - Swap two numbers using XOR without temp variable

---
+++ BitManipulation/XorSwap.java
@@ -0,0 +1,17 @@
+class XorSwap {
+
+    public static void swap(int[] arr) {
+        arr[0] = arr[0] ^ arr[1];
+        arr[1] = arr[0] ^ arr[1];
+        arr[0] = arr[0] ^ arr[1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 7 };
+
+        swap(arr);
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+}
