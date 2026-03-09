d7a01eb24441705e4efc435a65ec812401167af1
Alapati Abhinav Chowdhary <abhinavalapati.aac@gmail.com>
2026-03-09 23:16:59 +0530

    Add NoSetBits - Count set bits using Brian Kernighan's algorithm

---
+++ BitManipulation/NoSetBits.java
@@ -0,0 +1,16 @@
+class NoSetBits {
+    public static int nosetbits(int n) {
+        int c = 0;
+        while (n > 0) {
+            n &= (n - 1);
+            c++;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(nosetbits(n));
+
+    }
+}
