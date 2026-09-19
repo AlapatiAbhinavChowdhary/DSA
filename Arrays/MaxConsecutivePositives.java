cc65cf834318cec60247ef54969476de36a0d76a
Alapati Abhinav Chowdhary <147315664+AlapatiAbhinavChowdhary@users.noreply.github.com>
2026-09-19 15:53:41 +0530

    Add MaxConsecutivePositives class to find max positives

---
+++ Arrays/MaxConsecutivePositives.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class MaxConsecutivePositives {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
